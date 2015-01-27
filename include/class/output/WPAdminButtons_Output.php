9af6abd2ba89619b3e12048728618f460a8a172f
Michael Uno <michael@michaeluno.jp>
2015-01-28 00:26:00 +0900

    Refactored the code for simplicity.

--- include/class/output/WPAdminButtons_Output.php
+++ include/class/output/WPAdminButtons_Output.php
@@ -20,11 +20,11 @@ class WPAdminButtons_Output {
     public $aArguments = array(
         'label'     => 'Download',  // the text label
         'title'     => '',          // title attribute
-        'size'      => 'medium',          // accepts 'large', 'small', 'medium'
+        'size'      => 'medium',    // accepts 'large', 'small', 'medium'
         'type'      => 'button-primary',   // 'button-primary' or 'button-secondary'
-        'label_color'     => '',
-        'background_color'     => '',
-        'border_color'     => '',
+        'label_color'       => '',
+        'background_color'  => '',
+        'border_color'      => '',
         
         // Attributes
         'href'      => '',
@@ -39,15 +39,18 @@ class WPAdminButtons_Output {
      */
     public function __construct( $asArguments ) {
         
-        $this->aArguments = $this->_formatArguments( 
+        $this->aArguments = $this->_getFormattedArguments( 
             is_array( $asArguments ) 
                 ? $asArguments 
                 : array( $asArguments )
         );
         
     }
-    
-        private function _formatArguments( array $aArguments ) {
+        /**
+         * Formats an argument array.
+         * @return      array       The formatted argument array.
+         */
+        private function _getFormattedArguments( array $aArguments ) {
             
             $aArguments = $aArguments + $this->aArguments;
             return $aArguments;
@@ -59,46 +62,19 @@ class WPAdminButtons_Output {
      */
     public function get() {
         
-        // $_oUtil = new WPAdminButtons_AminPageFramework_WPUtility;
         $_sDivTagStyle = isset( $this->aArguments['style']['div'] )
             ? $this->aArguments['style']['div']
             : '';
-        $_sATagStyle = isset( $this->aArguments['style']['a'] )
-            ? $this->aArguments['style']['a']
-            : $this->aArguments['style'];
-        $_sType = esc_attr( $this->aArguments['type'] );
-
-        switch( $this->aArguments['size'] ) {
-            case 'large':
-                $this->aArguments['class'] .= ' button-hero';
-            break;
-            case 'small':
-                $this->aArguments['class'] .= ' button-small';
-            break;
-            case 'medium':
-            default:
-            break;
-        }
-        
-        if ( trim( $this->aArguments['label_color'] ) ) {
-            $_sATagStyle .= "; color:{$this->aArguments['label_color']}";
-        }
-        if ( trim( $this->aArguments['background_color'] ) ) {
-            $_sATagStyle .= "; background-color:{$this->aArguments['background_color']}";
-        }               
-        if ( trim( $this->aArguments['border_color'] ) ) {
-            $_sATagStyle .= "; border-color:{$this->aArguments['border_color']}";
-        }               
         
         return "<div class='wp-core-ui'>"
                 . "<div class='welcome-panel'" 
                         . " style='" . esc_attr( $_sDivTagStyle ) . "'"
                     . ">"
                     . "<a" 
-                        . " class='button {$_sType}" . esc_attr( $this->aArguments['class'] )  . "'"
+                        . " class='" . $this->_getClassAttribute( $this->aArguments )  . "'"
                         . " href='" . esc_attr( $this->aArguments['href'] )  . "'"
                         . " title='" . esc_attr( $this->aArguments['title'] )  . "'"
-                        . " style='" . esc_attr( $_sATagStyle )  . "'"
+                        . " style='" . esc_attr( $this->_getATagStyle( $this->aArguments ) )  . "'"
                         . " target='" . esc_attr( $this->aArguments['target'] )  . "'"
                         . " rel='" . esc_attr( $this->aArguments['rel'] )  . "'"
                     . ">"
@@ -109,5 +85,65 @@ class WPAdminButtons_Output {
         ;
         
     }
+ 
+        /**
+         * Returns the inline CSS rules applied to the 'a' tag of the button.
+         * 
+         * @return      string      Inline CSS rules.
+         */
+        private function _getATagStyle( array $aArguments ) {
+
+            $_sATagStyle = isset( $aArguments['style']['a'] )
+                ? $aArguments['style']['a']
+                : $aArguments['style'];    
+
+            if ( trim( $aArguments['label_color'] ) ) {
+                $_sATagStyle .= "; color:{$aArguments['label_color']}";
+            }
+            if ( trim( $aArguments['background_color'] ) ) {
+                $_sATagStyle .= "; background-color:{$aArguments['background_color']}";
+            }               
+            if ( trim( $aArguments['border_color'] ) ) {
+                $_sATagStyle .= "; border-color:{$aArguments['border_color']}";
+            }               
+            return $_sATagStyle;
+            
+        }        
+        
+        /**
+         * 
+         * @return      The calculated class attributes for the 'a' tab of the button.
+         */
+        private function _getClassAttribute( array $aArguments ) {
+
+            $_sClassAttribute   = 'button'
+                . ' ' . $aArguments['type'] 
+                . ' ' . $this->_getSizeClassSelector( $aArguments['size'] )
+                . ' ' . $aArguments['class'];
+                
+            return esc_attr( $_sClassAttribute );
+            
+        }        
+            /**
+             * Returns the class selector for the size.
+             * @return      string      The size specific class selector.
+             */
+            private function _getSizeClassSelector( $sSizeSlug ) {
+                
+                switch( $sSizeSlug ) {
+                    case 'large':
+                        $_sClass = 'button-hero';
+                        break;
+                    case 'small':
+                        $_sClass = 'button-small';
+                        break;
+                    case 'medium':
+                    default:
+                        $_sClass = '';
+                        break;
+                }
+                return $_sClass;
+                
+            }               
         
 }
